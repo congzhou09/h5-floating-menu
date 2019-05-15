a11a411b6d24d7f176f958113e2b08ff612ec1af
congzhou09 <yuancongzhou@yeah.net>
2019-05-15 17:05:06 +0800

    1.添加菜单项的渲染； 2.配置项整理； 3.babel增加class与export支持

--- src/index.js
+++ src/index.js
@@ -6,47 +6,136 @@ if(!window||!document)
     throw new Error('h5-floating-menu info: only support for browser!');
 }
 
-//读取配置
-let theConfig = (window.H5FloatingMenuConfig)?window.H5FloatingMenuConfig:defaultConfig;
-
 class H5FloatingMenu{
-    constructor(){
+    constructor(customConfig){
         this.containerDOM = document.createElement('div');
+        this.menuEntryDOM = null;
         this.maskDOM = document.createElement('div');
+        this.menuItemsDOM = null;
+        this.isDraggable = true;
+        this.theConfig =(!!customConfig&&typeof(customConfig)==='object')?Object.assign(defaultConfig, customConfig):defaultConfig;//读取配置
+    
+        //页面载入完成时执行
+        if(document.readyState === 'complete')
+        {
+            this.init();
+        }
+        else
+        {
+            window.addEventListener('load', ()=>{
+                this.init();
+            });
+        }
     }
     init(){
-        let theHtmlTemplate = `<div class=""></div>`;
+        let menuItemsHTML= `<ul style="position: absolute;display:none;padding:0;list-style-type:none;margin: calc(${this.theConfig.menuItemGap} + 1px) 0;">`;
+        for(let i=0;i<this.theConfig.menuItems.length;i++)
+        {
+            menuItemsHTML += `<div class="h5-floating-menu-item" style="" onclick="${this.theConfig.menuItems[i].callback}">
+                ${this.theConfig.menuItems[i].icon}
+                <span class="h5-floating-menu-item-text" style="position:absolute;width:max-content;">
+                    ${this.theConfig.menuItems[i].text}
+                </span>
+            </div>`;
+        }
+        menuItemsHTML += `</ul>`;
     
         //添加元素
-        this.containerDOM.className = 'h5-floating-menu-container';
-        this.containerDOM.innerHTML = theConfig.outerIcon;
+        this.containerDOM.className = 'h5-floating-menu-shell';
+        this.containerDOM.innerHTML = `<div class="h5-floating-menu-item">${this.theConfig.mainIconClose}</div>${menuItemsHTML}`;
+        this.containerDOM.style.cssText = '';
+        this.menuEntryDOM = this.containerDOM.querySelector('.h5-floating-menu-item:nth-child(1)');
+        this.menuItemsDOM = this.containerDOM.querySelector('ul:nth-of-type(1)');
         document.body.appendChild(this.containerDOM);
         
         this.maskDOM.className = 'h5-floating-menu-mask';
-        this.maskDOM.style.cssText = 'display:none;position:fixed;width:100%;height:100%;background-color:rgba(10, 10, 10, 0.69);top:0;left:0;z-index:10;';
+        this.maskDOM.style.cssText = `display:none;position:fixed;width:100%;height:100%;background-color:rgba(10, 10, 10, 0.69);top:0;left:0;z-index:${this.theConfig.zIndex};`;
         document.body.appendChild(this.maskDOM);
         //可拖动
         this.makeDraggable(this.containerDOM);
         //添加样式
-        if(theConfig.outerCssStyle)
-        {
-            let theStyleDOM = document.createElement('style');
-            theStyleDOM.type = 'text/css';
-            theStyleDOM.innerHTML = `.h5-floating-menu-container${theConfig.outerCssStyle}`;
-            document.head.appendChild(theStyleDOM);
+        let theStyleDOM = document.createElement('style');
+        theStyleDOM.type = 'text/css';
+        theStyleDOM.innerHTML = `
+        .h5-floating-menu-shell::after{
+            content:'';
+            display:block;
+            clear:both;
         }
+        .h5-floating-menu-shell{
+            position: fixed;
+            top: ${this.theConfig.initialTop};
+            left: ${this.theConfig.initialLeft};
+            z-index:${this.theConfig.zIndex+1};
+            display: flex;
+            flex-direction: column-reverse;
+            justify-content: space-between;
+        }
+        .h5-floating-menu-container-A, .h5-floating-menu-container-B{
+            top: ${this.theConfig.menuItemSize};
+        }
+        .h5-floating-menu-container-C, .h5-floating-menu-container-D{
+            bottom: ${this.theConfig.menuItemSize};
+        }
+        .h5-floating-menu-container-A .h5-floating-menu-item-text, .h5-floating-menu-container-C .h5-floating-menu-item-text{
+            left:calc(${this.theConfig.menuItemSize} + 10px);
+        }
+        .h5-floating-menu-container-B .h5-floating-menu-item-text, .h5-floating-menu-container-D .h5-floating-menu-item-text{
+            right:calc(${this.theConfig.menuItemSize} + 10px);
+        }
+        .h5-floating-menu-item{
+            width: ${this.theConfig.menuItemSize};
+            height: ${this.theConfig.menuItemSize};
+            background-color: ${this.theConfig.menuItemBackgroundColor};
+            color: ${this.theConfig.menuItemTextColor};
+            border-radius: 50%;
+            border: 1px solid #cfcccc;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: ${this.theConfig.menuItemGap} 0;
+        }`;
+        document.head.appendChild(theStyleDOM);
         //子菜单弹出
         let isMenuShow = false;
-        this.containerDOM.addEventListener('click', ()=>{
+        let halfWidth = (document.documentElement.clientWidth)/2;
+        let halfHeight = (document.documentElement.clientHeight)/2;
+        this.menuEntryDOM.addEventListener('click', (e)=>{
             if(!isMenuShow)
             {
                 this.showMenu();
+                this.menuItemsDOM.style.display = "block";
+                if(e.clientX< halfWidth)
+                {
+                    if(e.clientY< halfHeight)
+                    {
+                        this.menuItemsDOM.className = "h5-floating-menu-container-A";
+                    }
+                    else
+                    {
+                        this.menuItemsDOM.className = "h5-floating-menu-container-C";
+                    }
+                }
+                else
+                {
+                    if(e.clientY< halfHeight)
+                    {
+                        this.menuItemsDOM.className = "h5-floating-menu-container-B";
+                    }
+                    else
+                    {
+                        this.menuItemsDOM.className = "h5-floating-menu-container-D";
+                    }
+                }
                 isMenuShow = true;
+                this.isDraggable = false;
             }
             else
             {
                 this.hideMenu();
+                this.menuItemsDOM.style.display = "none";
                 isMenuShow = false;
+                this.isDraggable = true;
             }
         });
     }
@@ -54,7 +143,7 @@ class H5FloatingMenu{
         if(this.maskDOM)
         {
             this.maskDOM.style.display = "block";
-            this.containerDOM.innerHTML = theConfig.outerIconClose;
+            this.menuEntryDOM.innerHTML = this.theConfig.mainIconOpen;
         }
     }
     
@@ -62,7 +151,7 @@ class H5FloatingMenu{
         if(this.maskDOM)
         {
             this.maskDOM.style.display = "none";
-            this.containerDOM.innerHTML = theConfig.outerIcon;
+            this.menuEntryDOM.innerHTML = this.theConfig.mainIconClose;
         }
     }
     
@@ -86,7 +175,7 @@ class H5FloatingMenu{
             isMouseDown = true;
         };
         let moveFun = (e)=>{
-            if(isMouseDown)
+            if(isMouseDown&& this.isDraggable)
             {
                 let curX, curY;
                 if(e.type === 'touchmove')
@@ -129,16 +218,4 @@ class H5FloatingMenu{
     }
 }
 
-const h5FloatingMenu = new H5FloatingMenu();
-
-//页面载入完成时执行
-if(document.readyState === 'complete')
-{
-    h5FloatingMenu.init();
-}
-else
-{
-    window.addEventListener('load', ()=>{
-        h5FloatingMenu.init();
-    });
-}
+export default H5FloatingMenu;
